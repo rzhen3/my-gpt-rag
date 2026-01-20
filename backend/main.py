c557fb26cc2533ad5b3f96622a5be8c6f7bdd604
Roy Zheng <royzheng2003@gmail.com>
2026-01-20 17:40:27 -0500

    config stuff

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # "uvicorn main:app" where 'main' corresponds to file, 'app' corresponds to variable name
-app = FastAPI()
+app = FastAPI(
+    title = 'my-gpt-rag',
+    description = 'prompting environment project',
+    version = '0.1.0',
+    docs_url = '/docs',
+    redoc_url = '/redoc',
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+
 
 @app.get("/")
 def read_root():
@@ -10,3 +27,7 @@ def read_root():
 @app.get("/health")
 def health_check():
     return {"status": "health"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run('main:app', port=8000, reload = True)
