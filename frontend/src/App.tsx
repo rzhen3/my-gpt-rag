b12f929d59faff5a776b6a001b18b130bd59f58a
Roy Zheng <royzheng2003@gmail.com>
2026-01-21 00:47:51 -0500

    idk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import SimpleFlow from './components/SimpleFlow';
 function App() {
   return (
       <div className = "App">
+        <h2>
+          &lt; A better name than 'my-gpt-rag' &gt;
+        </h2>
         <SimpleFlow/>
       </div>
   );
